7cd0e4565106f994f25e369757e9cbab23003f45
danymukesha <danymukesha@gmail.com>
2024-02-11 19:30:54 +0100

    add visualizations

---
+++ R/plot_population.R
@@ -0,0 +1,18 @@
+#' Plot Fitness Values
+#'
+#' Plot the fitness values of the population over generations.
+#'
+#' @param fitness_values A numeric vector containing fitness values.
+#'
+#' @examples
+#' # example of usage
+#' fitness_values <- c(10, 8, 6, 4, 2)
+#' plot_fitness(fitness_values)
+#'
+#' @export
+plot_fitness <- function(fitness_values) {
+  generations <- seq_along(fitness_values)
+  plot(generations, fitness_values, type = "l",
+       xlab = "Generations", ylab = "Fitness",
+       main = "Fitness Values Over Generations")
+}
